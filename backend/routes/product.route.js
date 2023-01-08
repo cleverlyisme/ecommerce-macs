ba06f8008e6ad161b2a070c2c6605956ec4178a2
cleverlysisme <tuananhdao2862@gmail.com>
2023-01-09 04:08:32 +0700

    chore: remove middleware in route get product

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,8 +5,8 @@ const productController = require("../controllers/product.controller");
 
 const productRoute = express.Router();
 
-productRoute.get("/products", auth(), productController.getProducts);
-productRoute.get("/products/:id", auth(), productController.getById);
+productRoute.get("/products", productController.getProducts);
+productRoute.get("/products/:id", productController.getById);
 productRoute.post("/products", auth(), productController.createProduct);
 productRoute.put("/products/:id", auth(), productController.updateProduct);
 productRoute.delete("/products/:id", auth(), productController.deleteProduct);

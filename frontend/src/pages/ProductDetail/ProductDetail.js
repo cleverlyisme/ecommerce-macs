c5f7e8cba17dfd6c9191e46e476049cf438614ba
jack <jack.nguyen@weareday.one>
2023-01-19 15:03:10 +0700

    fix: responsive product detail

--- frontend/src/pages/ProductDetail/ProductDetail.js
+++ frontend/src/pages/ProductDetail/ProductDetail.js
@@ -1,20 +1,20 @@
-import { Container, Row, Col, Button } from "reactstrap";
-import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Container, Row, Col, Button } from 'reactstrap';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   NotificationContainer,
   NotificationManager,
-} from "react-notifications";
-import ReactStars from "react-rating-stars-component";
+} from 'react-notifications';
+import ReactStars from 'react-rating-stars-component';
 
-import Header from "../../components/Header";
-import Layout from "../../components/Layout";
-import { getProductById } from "../../services/products.service";
-import Currency from "../../utils/formatCurrency";
-import useAppContext from "../../hooks/useAppContext";
-import formatFileUrl from "../../utils/formatFileUrl";
-import RelatedProducts from "./Components/RelatedProducts";
+import Header from '../../components/Header';
+import Layout from '../../components/Layout';
+import { getProductById } from '../../services/products.service';
+import Currency from '../../utils/formatCurrency';
+import useAppContext from '../../hooks/useAppContext';
+import formatFileUrl from '../../utils/formatFileUrl';
+import RelatedProducts from './Components/RelatedProducts';
 
 const ProductDetail = () => {
   const navigate = useNavigate();
@@ -55,11 +55,11 @@ const ProductDetail = () => {
         <Col xs={12} sm={12} md={6}>
           <img
             src={formatFileUrl(images[index])}
-            style={{ maxWidth: "100%" }}
+            style={{ maxWidth: '100%' }}
           />
           <div
-            className="d-flex align-items-center"
-            style={{ overflowY: "auto", gap: 15, maxWidth: "100%" }}
+            className="mt-2 d-flex align-items-center"
+            style={{ overflowY: 'auto', gap: 15, maxWidth: '100%' }}
           >
             {images.map((img, i) => {
               return (
@@ -68,10 +68,10 @@ const ProductDetail = () => {
                     key={img}
                     src={formatFileUrl(img)}
                     style={{
-                      width: "100%",
-                      objectFit: "cover",
-                      cursor: "pointer",
-                      border: i === index ? "1px solid #888888" : 0,
+                      width: '100%',
+                      objectFit: 'cover',
+                      cursor: 'pointer',
+                      border: i === index ? '1px solid #888888' : 0,
                     }}
                     onClick={() => setIndex(i)}
                   />
@@ -80,7 +80,7 @@ const ProductDetail = () => {
             })}
           </div>
         </Col>
-        <Col xs={6} className="d-flex flex-column" style={{ gap: 30 }}>
+        <Col xs={12} md={6} className="d-flex flex-column" style={{ gap: 30 }}>
           <div className="d-flex flex-column" style={{ gap: 5 }}>
             <div style={{ fontSize: 20, fontWeight: 600 }}>{name}</div>
             <div style={{ fontSize: 10, opacity: 0.7 }}>{description}</div>
@@ -91,7 +91,7 @@ const ProductDetail = () => {
               <div className="d-flex align-items-center" style={{ gap: 20 }}>
                 <img
                   src="/minus.png"
-                  style={{ cursor: "pointer", width: 25, height: 25 }}
+                  style={{ cursor: 'pointer', width: 25, height: 25 }}
                   onClick={() =>
                     count > 1 ? setCount(count - 1) : setCount(count)
                   }
@@ -99,7 +99,7 @@ const ProductDetail = () => {
                 <div>{count}</div>
                 <img
                   src="/add.png "
-                  style={{ cursor: "pointer", width: 25, height: 25 }}
+                  style={{ cursor: 'pointer', width: 25, height: 25 }}
                   onClick={() =>
                     count < quantity ? setCount(count + 1) : setCount(count)
                   }
@@ -108,7 +108,7 @@ const ProductDetail = () => {
               <div>
                 <div className="d-flex" style={{ gap: 3 }}>
                   <div style={{ fontSize: 12 }}>Chỉ còn </div>
-                  <div style={{ color: "#ec9c63", fontSize: 12 }}>
+                  <div style={{ color: '#ec9c63', fontSize: 12 }}>
                     {quantity} sản phẩm
                   </div>
                   <div style={{ fontSize: 12 }}> còn lại</div>
@@ -126,10 +126,10 @@ const ProductDetail = () => {
 
           <Button
             color="success"
-            style={{ width: "50%" }}
+            style={{ width: '50%' }}
             onClick={() => {
               updateProduct({ _id: id, quantity: count, name, price });
-              NotificationManager.success("Add to cart successfully");
+              NotificationManager.success('Add to cart successfully');
             }}
           >
             Thêm vào giỏ hàng
@@ -146,8 +146,8 @@ const ProductDetail = () => {
         {relatedProducts.map((product) => {
           return (
             <Col className="p-2" xs={6} sm={4} md={3} lg={2} xl={2}>
-              {" "}
-              <RelatedProducts product={product} />{" "}
+              {' '}
+              <RelatedProducts product={product} />{' '}
             </Col>
           );
         })}
